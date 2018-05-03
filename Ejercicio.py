0bdb6662ca820b27e0f8f65bd3363bbd407845ed
Emerick95 <38759779+Emerick95@users.noreply.github.com>
2018-05-02 19:54:01 -0500

    Update Ejercicio.py

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,3 +1,5 @@
+import math
+
 #funciones 
 def vol_pircuadrada():
     print ("Ingresa")
@@ -6,6 +8,15 @@ def vol_pircuadrada():
     areab=lado_base*lado_base
     vol=(areab*altura)/3
     print ("El volumen es ",vol," cm3")
+    
+def Funcion1():
+    r=float(input("Ingrese el radio de la esfera : "))
+    print("Radio: ",r , "m")
+    v=4/3*math.pi*pow(r,3)
+
+    print("El volumen de la esfera es: ", "%1.2f" %v , "m3")
+    
+    
 #codigo
 var=1
 op=0
@@ -26,7 +37,7 @@ while (var!=0):
         var=0
     elif op==4:
         print("Seleccionaste Esferar")
-        
+        Funcion1()
         var=0
     elif op==5:
         print ("GRACIAS")
